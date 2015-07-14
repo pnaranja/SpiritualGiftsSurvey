498a425b861c463cb54ac4881cea6d57f1bbb3f9
pnaranja <paul.naranja@gmail.com>
2015-07-13 17:04:35 -0700

    Questions now in the javascript code.  Able to output the questions inside the div

--- questions.js
+++ questions.js
@@ -1,29 +1,94 @@
+var the_questions = {
+1 : 'I have the ability to organize ideas, resources, time, and people effectively.',
+2 : 'I am willing to study and prepare for the task of teaching.',
+3 : 'I am able to relate the truths of God to specific situations.',
+4 : 'I have a God-given ability to help others grow in their faith.',
+5 : 'I possess a special ability to communicate the truth of salvation.',
+6 : 'I have the ability to make critical decisions when necessary.',
+7 : 'I am sensitive to the hurts of people.',
+8 : 'I experience joy in meeting needs through sharing possessions.',
+9 : 'I enjoy studying.',
+10 : 'I have delivered God\'s message of warning and judgment.',
+11 : 'I am able to sense the true motivation of persons and movements.',
+12 : 'I have a special ability to trust God in difficult situations.',
+13 : 'I have a strong desire to contribute to the establishment of new churches.',
+14 : 'I take action to meet physical and practical needs rather than merely talking about or planning to help.',
+15 : 'I enjoy entertaining guests in my home.',
+16 : 'I can adapt my guidance to fit the maturity of those working with me.',
+17 : 'I can delegate and assign meaningful work.',
+18 : 'I have an ability and desire to teach.',
+19 : 'I am usually able to analyze a situation correctly.',
+20 : 'I have a natural tendency to encourage others.',
+21 : 'I am willing to take the initiative in helping other Christians grow in their faith.',
+22 : 'I have an acute awareness of the emotions of other people, such as loneliness, pain, fear, and anger.',
+23 : 'I am a cheerful giver.',
+24 : 'I spend time digging into facts.',
+25 : 'I feel that I have a message from God to deliver to others.',
+26 : 'I can recognize when a person is genuine/honest.',
+27 : 'I am a person of vision (a clear mental portrait of a preferable future given by God). I am able to communicate vision in such a way that others commit to making the vision a reality.',
+28 : 'I am willing to yield to God\'s will rather than question and waver.',
+29 : 'I would like to be more active in getting the gospel to people in other lands.',
+30 : 'It makes me happy to do things for people in need.',
+31 : 'I am successful in getting a group to do its work joyfully.',
+32 : 'I am able to make strangers feel at ease.',
+33 : 'I have the ability to plan learning approaches.',
+34 : 'I can identify those who need encouragement.',
+35 : 'I have trained Christians to be more obedient disciples of Christ.',
+36 : 'I am willing to do whatever it takes to see others come to Christ.',
+37 : 'I am attracted to people who are hurting.',
+38 : 'I am a generous giver.',
+39 : 'I am able to discover new truths.',
+40 : 'I have spiritual insights from Scripture concerning issues and people that compel me to speak out.',
+41 : 'I can sense when a person is acting in accord with God\'s will.',
+42 : 'I can trust in God even when things look dark.',
+43 : 'I can determine where God wants a group to go and help it get there.',
+44 : 'I have a strong desire to take the gospel to places where it has never been heard.',
+45 : 'I enjoy reaching out to new people in my church and community.',
+46 : 'I am sensitive to the needs of people.',
+47 : 'I have been able to make effective and efficient plans for accomplishing the goals of a group.',
+48 : 'I often am consulted when fellow Christians are struggling to make difficult decisions.',
+49 : 'I think about how I can comfort and encourage others in my congregation.',
+50 : 'I am able to give spiritual direction to others.',
+51 : 'I am able to present the gospel to lost persons in such a way that they accept the Lord and His salvation.',
+52 : 'I possess an unusual capacity to understand the feelings of those in distress.',
+53 : 'I have a strong sense of stewardship based on the recognition that God owns all things.',
+54 : 'I have delivered to other persons messages that have come directly from God.',
+55 : 'I can sense when a person is acting under God\'s leadership.',
+56 : 'I try to be in God\'s will continually and be available for His use.',
+57 : 'I feel that I should take the gospel to people who have different beliefs from me.',
+58 : 'I have an acute awareness of the physical needs of others.',
+59 : 'I am skilled in setting forth positive and precise steps of action.',
+60 : 'I like to meet visitors at church and make them feel welcome.',
+61 : 'I explain Scripture in such a way that others understand it.',
+62 : 'I can usually see spiritual solutions to problems.',
+63 : 'I welcome opportunities to help people who need comfort, consolation, encouragement, and counseling.',
+64 : 'I feel at ease in sharing Christ with nonbelievers.',
+65 : 'I can influence others to perform to their highest God-given potential.',
+66 : 'I recognize the signs of stress and distress in others.',
+67 : 'I desire to give generously and unpretentiously to worthwhile projects and ministries.',
+68 : 'I can organize facts into meaningful relationships.',
+69 : 'God gives me messages to deliver to His people.',
+70 : 'I am able to sense whether people are being honest when they tell of their religious experiences.',
+71 : 'I enjoy presenting the gospel to persons of other cultures and backgrounds.',
+72 : 'I enjoy doing little things that help people.',
+73 : 'I can give a clear, uncomplicated presentation.',
+74 : 'I have been able to apply biblical truth to the specific needs of my church.',
+75 : 'God has used me to encourage others to live Christlike lives.',
+76 : 'I have sensed the need to help other people become more effective in their ministries.',
+77 : 'I like to talk about Jesus to those who do not know Him.',
+78 : 'I have the ability to make strangers feel comfortable in my home.',
+79 : 'I have a wide range of study resources and know how to secure information.',
+80 : 'I feel assured that a situation will change for the glory of God even when the situation seem impossible.'};
+
+
 
 var message = function(msg,qnum){
   var container  = document.createElement('div');
   container.id = 'q_' + qnum;
-  container.appendChild(document.createTextNode(msg));
+  container.appendChild(document.createTextNode(qnum+". "+msg));
 
-  return container;
+  return container.outerHTML;
 };
 
-function readFile(evt){
-  var f = evt.target.files[0];
-
-  if(f){
-        var r = new FileReader();
-
-        r.onload = function(e){
-        var contents = e.target.result;
-        alert("Got file " + f.name);
-        document.getElementById('questions').innerHTML = "<div>hello!</div>"; //message(r.result,1);
-        };
-
-        r.readAsText(f);
-
-    }
-  else
-    alert('Failed to load file');
-}
-
-  document.getElementById('the-file').addEventListener('change' ,readFile, false);
+for (var i in the_questions)
+  document.getElementById('questions').innerHTML += message(the_questions[i],i);
